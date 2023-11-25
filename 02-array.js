c3ec827c934e08f3513f122faef0f7859a99e85e
YokimSl <jhonnata1337@gmail.com>
2023-11-24 21:20:45 -0300

    desafio two completed

--- 02-array.js
+++ 02-array.js
@@ -1,11 +1,25 @@
-// 1- Crie um array vazio chamado animals
+// 1- Crie um array vazio chamado animals ok
 
-// 2- Adicione nesse array dois animais: dog e cat (você pode fazer isso de mais de uma forma diferente!)
+// 2- Adicione nesse array dois animais: dog e cat (você pode fazer isso de mais de uma forma diferente!) ok
 
-// 3- Adicione no começo do array outro animal: spider
+// 3- Adicione no começo do array outro animal: spider. ok
 
-// 4- Remova o primeiro elemento do array
+// 4- Remova o primeiro elemento do array. ok
 
-// 5- Remova o último elemento do array
+// 5- Remova o último elemento do array ok
 
 // 6- Como ficou o array final?! E qual é o elemento 1 desse array?
+
+
+const animals = [
+    "dog",
+    "cat",
+];
+
+animals.unshift("spider"),
+
+animals.shift(),
+
+animals.pop(),
+
+console.log(animals)
