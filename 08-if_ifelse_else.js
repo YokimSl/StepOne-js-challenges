e746e05f54f6e6701a2b3e848441b97449635caf
YokimSl <jhonnata1337@gmail.com>
2023-12-01 22:20:21 -0300

    if else completed

--- 08-if_ifelse_else.js
+++ 08-if_ifelse_else.js
@@ -39,13 +39,13 @@ let num3 = 40;
 
 function findLargest(num1, num2, num3) {
     if (num1 > num2 && num1 > num3) {
-        console.log(num1);
+        console.log(`${num1} is the largest number`);
     } else if (num2 > num1 && num2 > num3) {
-        console.log(num2);
-    } else if (num3 > num1 && num2) {
-        console.log(num3);
+        console.log(`${num2} is the largest number`);
+    } else if (num3 > num1 && num3 > num2) {
+        console.log(`${num3} is the largest number` );
     } else {
-        console.log();
+        console.log("Two or more numbers are equal");
     }
 }
 
