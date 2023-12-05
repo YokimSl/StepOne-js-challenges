729580e88139a8ca5050ef48f5be7a096f126eb6
YokimSl <jhonnata1337@gmail.com>
2023-12-04 22:02:50 -0300

    rectangleArea completed

--- 10-funcoes.js
+++ 10-funcoes.js
@@ -5,3 +5,9 @@
 // A função deve mostrar no console a área do retângulo com a seguinte frase:
 // The area of rectangle is XXX.
 
+function rectangleArea (length, width){
+    const area = length * width / 2 ;
+    console.log(area)
+
+}
+rectangleArea(2, 2)
